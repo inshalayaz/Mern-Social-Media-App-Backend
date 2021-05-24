8cf277ec59a53b753b52032443ec49938e596a23
inshal ayaz <inshal.ayaz14@gmail.com>
2021-05-24 20:37:55 +0500

    make it better

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const dotenv = require('dotenv')
 
 const postRoutes = require('./routes/posts')
 
 const app = express();
 
-
+dotenv.config()
 app.use(bodyParser.json({limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true }))
 app.use(cors())
@@ -16,11 +17,11 @@ app.use('/posts', postRoutes)
 
 // Connecting to DataBase
 
-const CONNECTION_URL = 'mongodb+srv://inshalayaz:inshal14@cluster0.zopu7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then( () => app.listen(PORT, () => console.log(`Server running on ${PORT}`) ))
     .catch(error => console.log(error.message))
 
